3c4b5948c7a82ac5e54c3f2b0cb2831711c4518c
dipayansadhukhan <dipayans0004@gmail.com>
2025-04-03 01:59:34 +0530

    uptading links

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("home").addEventListener("click",()=>{
+    window.location.href = "index.html";
+})
+document.getElementById("about").addEventListener("click",()=>{
+    window.location.href = "about.html";
+})
+document.getElementById("contactUs").addEventListener("click",()=>{
+    window.location.href = "contact.html";
+})
+document.getElementById("support").addEventListener("click",()=>{
+    window.location.href = "support.html";
+})
+document.getElementById("writeForUs").addEventListener("click",()=>{
+    window.location.href = "writeforus.html";
+})
